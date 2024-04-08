f23c1cb554f5f77242dbe9baf6b52db00d0f0c78
isabellyschaffer <163335686+isabellyschaffer@users.noreply.github.com>
2024-04-08 17:21:02 -0300

    EXERCICIOS

---
+++ EXERCICIO/rafa03.js
@@ -0,0 +1,82 @@
+// EXERCICIOS 03-04
+
+
+// // 1-  mostra os numeros ate 100 
+// for(let i=0; i<=100; i++){
+//     console.log(i)
+// }
+
+
+// // 2- imprimir a tabuada do 5
+// let tabuada, resultado = ""
+// tabuada = 5
+
+// for(let i = 1; i<= 10; i++){
+//     // 1 result +=tabuada + " x " + i + " = " + (tabuada * i) + "\n" 
+//      resultado += `${tabuada} x ${i} = ${tabuada*i}
+// `
+// }
+// alert(resultado)
+
+
+
+
+// 3- imprimir a tabuada que o usuario pedir
+// let tabuada, resultado = ""
+
+// tabuada = Number(prompt("Digite um numero"))
+
+// for(let i = 1; i<= 20; i++){
+//     // 1 result +=tabuada + " x " + i + " = " + (tabuada * i) + "\n" 
+//      resultado += `${tabuada} x ${i} = ${tabuada*i}
+// `
+// }
+// alert(resultado)
+
+
+
+
+
+// 4- imprimir de 10 a 0
+// for(let i = 10; i>= 1; i--){
+//     console.log (i)
+// }
+
+
+
+
+// 5- impar de ate 20
+// como colocar isso no html?
+
+// for(let i=1; i<=20; i+=2){
+//    console.log (i)
+// } 
+
+
+
+
+
+// 6- Faça um programa que calcule o quadrado de cada número de 1 a 10.
+// for(let i=1; i <=10; i++){
+//     let quadrado = i**2
+//         console.log("O quadrado de ", i,"é", quadrado)
+//     }
+ 
+
+
+
+
+// 7- Imprima os números de 1 a 10 em ordem crescente, depois imprima-os em ordem decrescente.
+// for(let i=0; i<=10; i++){
+//         console.log(i)
+//         }
+//     for(let i=9; i>=0; i--){
+//         console.log(i)
+//         }
+
+
+
+
+
+
+      
